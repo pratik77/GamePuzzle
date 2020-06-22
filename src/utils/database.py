44b0dc51faa679a536b4785aee8af01633b0a5cb
ppsahoo <58328206+ppsahoo@users.noreply.github.com>
2020-06-22 14:11:01 +0530

    Checking for postgre commented

--- src/utils/database.py
+++ src/utils/database.py
@@ -7,8 +7,8 @@ from flask_migrate import Migrate
 
 app = FlaskAPI(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:rootpass@localhost:3306/puzzle_game'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:admin@localhost:5432/puzzle_game'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:rootpass@localhost:3306/puzzle_game'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:admin@localhost:5432/puzzle_game'
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///covid4New.db'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db, compare_type=True)
