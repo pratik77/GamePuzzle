cd63ff23628c3bae89ea1cb4588a6d47a5c9cff3
zyrus <pratik77sahoo@gmail.com>
2020-06-22 13:40:28 +0530

    added game name generator (#1)
    Co-authored-by: pratiks77 <pekkagolem@77>

--- src/GameApis.py
+++ src/GameApis.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 from api.Submit import Submit
+from api.GenerateGameName import GenerateGameName
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate   
 from utils.database import *
@@ -16,9 +17,9 @@ from models import model
 api = Api(app)
 # app.config['SECRET_KEY'] = b"\x9c\x9a\xd7qam\x95W\xeb\xbc\x88O'T\x12\\\x99\x11\n[\xfd\xaa\rL"
 
-# CORS(app)
+CORS(app)
 api.add_resource(Submit, '/submit')
-
+api.add_resource(GenerateGameName, '/generateGameName')
 if __name__ == '__main__':
 
     confFile = None
