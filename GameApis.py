6bab1d9f2b3a35f4ae644561a37152120a2104bd
pratiks77 <pekkagolem@77>
2020-06-28 17:18:22 +0530

    make it better

---
+++ GameApis.py
@@ -0,0 +1,49 @@
+from flask import Flask
+from flask_restful import Api
+from api.Submit import Submit
+from api.SubmitAnswer import SubmitAnswer
+from api.GiveUpQuestion import GiveUpQuestion
+from flask_cors import CORS, cross_origin
+from api.GetUserGamePlayData import GetUserGamePlayData
+from api.StartCompetition import StartCompetition
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate   
+from utils.database import *
+import configparser
+import getopt
+import logging
+import time
+from models import model
+from api.GetLeaderboard import GetLeaderboard
+from api.GetSubmissionDetailsAndLeaderboard import GetSubmissionDetailsAndLeaderboard
+
+# app = Flask(__name__)
+# api = Api(app)
+
+api = Api(app)
+# app.config['SECRET_KEY'] = b"\x9c\x9a\xd7qam\x95W\xeb\xbc\x88O'T\x12\\\x99\x11\n[\xfd\xaa\rL"
+
+CORS(app, support_credentials=True)
+api.add_resource(Submit, '/submit')
+api.add_resource(GetUserGamePlayData, '/getUserGamePlayData')
+api.add_resource(GetLeaderboard, '/getLeaderboard')
+api.add_resource(GetSubmissionDetailsAndLeaderboard, '/getSubmissionDetailsAndLeaderboard')
+api.add_resource(GiveUpQuestion, '/giveUpQuestion')
+api.add_resource(SubmitAnswer, '/submitAnswer')
+api.add_resource(StartCompetition,'/startCompetition')
+if __name__ == '__main__':
+
+    confFile = None
+    # handle the command line parameter to receive the configuration file
+    # try:
+
+    #     # confFile = "conf/setting.cfg"
+    # except:
+
+    #     print("errorCode ")
+    #     sys.exit(1)
+    try:
+        # cmdConfig = configparser.ConfigParser()
+        app.run(debug=True, host='0.0.0.0', port=5000, threaded=True)
+    except Exception as err:
+        print(err)
